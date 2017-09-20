4259cfec9377731e2c8472ea0dbf0462c59f7e57
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-20 21:19:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,16 +3,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  document.body.addEventListener('keydown', function(e)){
-  const key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
-      index++;
-      if (index === code.length) {
-        alert("Hurray!");
-        index = 0;
-      }
-    } else {
-      index = 0;
+    document.body.addEventListener('keydown', function(e) {
+        function onKeyDownHandler(e) {
+            const key = parseInt(e.detail || e.which);
+            if (key === code[index]) {
+                index++;
+                if (index === code.length) {
+                    alert("hurray!");
+                    index =0;
+                }
+            } else {
+                index = 0;)
+            }
+        }
     }
-  }
-}
