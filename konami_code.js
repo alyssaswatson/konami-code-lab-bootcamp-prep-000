e245fc247b6930fb0677ff1ad62e2cc60034c98e
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-20 18:09:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.body.addEventListener('keydown', function(event)){
+  document.body.addEventListener('keydown', function(e)){
+  function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
     if (key === code[idex]) {
       index++;
       if (index === code.length) {
