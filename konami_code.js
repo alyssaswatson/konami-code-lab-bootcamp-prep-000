78c79e21368108bb37cbde2b5c39f6df1b8e3a58
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-20 18:07:26 +0000

    automatically backed up by learn

---
+++ konami_code.js
@@ -0,0 +1,16 @@
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+function init() {
+  let index = 0;
+  document.body.addEventListener('keydown', function(event)){
+    if (key === code[idex]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index =0;
+    }
+  }
+}
